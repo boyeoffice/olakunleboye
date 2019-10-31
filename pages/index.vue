85ead676c3cfe93590c370dfda369c231fd7b099
Olakunle Boye <kunlexzy@gmail.com>
2019-10-31 02:51:10 +0100

    text position fixed on index

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,11 @@
 		<h1>
 			<span>H</span><span>i</span><span>,</span>
 			<br />
-			<span>I</span><span>'</span><span>m</span>
-			<span>B</span><span>o</span><span>y</span><span>e</span>
+			<span>I</span><span>’</span><span>m</span>
+			<span>B</span><span>o</span><span>y</span><span>e</span><span>,</span>
 			<br />
 			<span>w</span><span>e</span><span>b</span>
-			<span>d</span><span>e</span><span>v</span><span>e</span><span>l</span><span>o</span><span>l</span><span>o</span><span>p</span><span>e</span><span>r</span>
+			<span>d</span><span>e</span><span>v</span><span>e</span><span>l</span><span>o</span><span>p</span><span>e</span><span>r</span><span>.</span>
 		</h1>
 	</div>
   </div>
