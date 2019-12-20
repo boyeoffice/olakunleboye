0d325331347204fa45f25bdf3af7552bcae1140e
Olakunle Boye <kunlexzy@gmail.com>
2019-12-20 20:50:25 +0100

    Navbar responsive to mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,19 @@
 <template>
 	<div class="navbar">
 		<nuxt-link to='/' class="logo">
-		<img src="/img/logo.png" alt="">
-		<img src="/img/logo_sub.png" alt="">
+		<img src="/img/logo.png" alt="Olakunle Boye">
+		<img src="/img/logo_sub.png" alt="Olakunle Boye" style="width: 27px;">
 		</nuxt-link>
 		<nav :class="{'en': true, 'show': show}">
 			<nuxt-link :rel="link.name" :to="link.url" v-for="(link, index) in links" :key="index">
 				<i :class="link.icon"></i>
 			</nuxt-link>
 		</nav>
+		<ul>
+			<li v-for="link in social">
+				<a :href="link.url"> <i :class="link.icon"></i> </a>
+			</li>
+		</ul>
 		<a @click="show = !show" class="toggle" href="javascript:void(0)">
 			<i class="fa fa-bars"></i>
 		</a>
@@ -22,7 +27,8 @@
 			social: [
 			{name: 'Twitter', url: 'https://www.twitter.com/dev_boye', icon: 'fa fa-twitter'},
 			{name: 'Github', url: 'https://www.github.com/boyeoffice', icon: 'fa fa-github'},
-			{name: 'LinkedIn', url: '#', icon: 'fa fa-linkedin'}
+			{name: 'LinkedIn', url: 'https//linkedin.com/in/devboye', icon: 'fa fa-linkedin'},
+			{name: 'Instagram', url: 'https//instagram.com/in/devboye', icon: 'fa fa-instagram'}
 		],
 			links: [
 				{name: 'home', url: '/', icon: 'icon-hom'},
