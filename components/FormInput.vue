fa947dbab3bfee367a882eef7e09f6957fb28d7a
Olakunle Boye <kunlexzy@gmail.com>
2020-02-26 14:54:51 +0100

    contact form initiated

---
+++ components/FormInput.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <input :type="formType" class="form-control" :placeholder="placeHolder">
+</template>
+
+<script>
+export default {
+  props: {
+    formType: String,
+    placeHolder: String
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,46 +1,54 @@
-<template>
-  <div class="container contact">
-    <div class="content">
-      <h1 class="blast-root">
-        <blast-root :content="contact" />
-      </h1>
-      <p>I am interested in freelance opportunities – especially ambitious or large projects. However, if you have other request or question, don’t hesitate to contact me using below form either.</p>
-      <p></p>
-      <div class="contact-form">
-        <form @submit.prevent="Send" id="contact" autocomplete="off">
-          <ul><li class="half animated fadeInUp">
-          <input class="input__field input__field--hoshi" placeholder="Name" type="text" name="name">
-          <label class="input__label input__label--hoshi input__label--hoshi-color-2"></label></li>
-          <li class="half animated fadeInUp">
-          <input class="input__field input__field--hoshi" placeholder="Email" type="email" name="email">
-           <label class="input__label input__label--hoshi input__label--hoshi-color-2"></label></li>
-           <li class="animated fadeInUp">
-           <input class="input__field input__field--hoshi" placeholder="Subject" type="text" name="subject">
-           <label class="input__label input__label--hoshi input__label--hoshi-color-2"></label></li>
-           <li class="animated fadeInUp">
-           <textarea class="input__field input__field--hoshi" placeholder="Message" name="msg"></textarea>
-           <label class="input__label input__label--hoshi input__label--hoshi-color-2"></label></li>
-           <li style="display: inline-block;" class="submit animated fadeInUp">
-           <input type="hidden" name="ip" value="">
-           <input id="submit" type="submit" class="flat-button" value="HIT ME"></li>
-           <li class=" animated fadeInUp" style="float: right; color: #fff; margin-top: 5px;">or <a style="color: #fff; text-decoration: underline;" href="mailto:kunlexzy@gmail.com"> send me an email </a>instead.</li>
-           </ul>
-           <div></div>
-        </form>
-        </div>
-    </div>
+<template lang="html">
+  <div class="contact">
+    <b-container>
+      <b-row>
+          <b-col cols="11" md="8" lg="6"  class="content">
+            <h1 class="blast-root" aria-label=" About me ">
+              <blast-root :content="contact" />
+            </h1>
+            <p class="text-white">I am interested in freelance opportunities – especially ambitious or large projects. However, if you have other request or question, don’t hesitate to contact me using below form either.</p>
+            <div class="mt-2">
+              <b-form>
+                <div class="form-group row">
+                  <b-col>
+                    <form-input formType="text" placeHolder="Name"/>
+                  </b-col>
+                  <b-col>
+                    <form-input formType="email" placeHolder="Email"/>
+                  </b-col>
+                </div>
+                <div class="form-group">
+                  <form-input formType="text" placeHolder="Subject" />
+                </div>
+                <div class="form-group">
+                  <textarea rows="5" class="form-control" placeholder="Message"></textarea>
+                </div>
+                <button type="button" class="btn btn-sm btn-primary">HIT ME</button>
+                <span>
+                  or
+                  <b-link href="mailto:kunlexzy@gmail.com">send me an email</b-link>
+                </span>
+              </b-form>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+
   </div>
 </template>
 
 <script>
   import BlastRoot from '~/components/BlastRoot'
+  import FormInput from '~/components/FormInput'
   export default {
     components: {
-      BlastRoot
+      BlastRoot,
+      FormInput
     },
     data() {
       return {
-        contact: ['C','o','n','t','a','c','t']
+        contact: ['C','o','n','t','a','c','t'],
+        form: {}
       }
     },
     created() {
@@ -58,3 +66,6 @@
     }
   }
 </script>
+
+<style lang="css" scoped>
+</style>
